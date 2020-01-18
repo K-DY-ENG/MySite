434b6b9e6c43db65267dbc8be4867f81c77d5f29
Daniel <danielkarimov2016@yandex.ru>
2020-01-19 01:30:11 +0300

    First commit

---
+++ Server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from Weather import weather_by_city
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    weather = weather_by_city('Moscow,Russia')
+    if weather:
+        return f"Температура: {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
+    else:
+        return "Сервис погоды временно недоступен"
+if __name__ == "__main__":
+    app.run(debug=True)
